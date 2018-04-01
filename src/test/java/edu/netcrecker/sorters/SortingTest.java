6afca558ecb0b6efc32c8a1b660476e927f2b18e
anntsit <anatsit98@ukr.net>
2018-04-01 22:14:16 +0300

    add tests

---
+++ src/test/java/edu/netcrecker/sorters/SortingTest.java
@@ -0,0 +1,82 @@
+package edu.netcrecker.sorters;
+
+import edu.netcrecker.fillers.Generic;
+import org.junit.*;
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class SortingTest {
+    private ArrayList<Integer> originalArray, expectedArray, actualArray, arrayToOperate;
+    private int capacity;
+
+    @Before
+    public void init() {
+        capacity = 50;
+        originalArray = Generic.arrayRandom(capacity);
+        expectedArray = new ArrayList<>(capacity);
+        arrayToOperate = new ArrayList<>(capacity);
+    }
+
+    void fillArrays() {
+        Collections.copy(originalArray, expectedArray);
+        Collections.copy(originalArray, arrayToOperate);
+    }
+
+
+    @Test
+    public void ArraySortTest() {
+        ArraySort arrSort = new ArraySort();
+        fillArrays();
+        Collections.sort(expectedArray);
+        actualArray = arrSort.sort(arrayToOperate);
+
+        assertEquals(expectedArray, actualArray);
+    }
+
+    @Test
+    public void exchangeSortSort() {
+        ExchangeSort exSort = new ExchangeSort();
+        fillArrays();
+        Collections.sort(expectedArray);
+        actualArray = exSort.sort(arrayToOperate);
+
+        assertEquals(expectedArray, actualArray);
+    }
+    @Test
+    public void minimumSortSort() {
+        MinimumSort minSort = new MinimumSort();
+        fillArrays();
+        Collections.sort(expectedArray);
+        actualArray = minSort.sort(arrayToOperate);
+
+        assertEquals(expectedArray, actualArray);
+    }
+    @Test
+    public void quickSortSort() {
+        QuickSort qSort = new QuickSort();
+        fillArrays();
+        Collections.sort(expectedArray);
+        actualArray = qSort.sort(arrayToOperate);
+
+        assertEquals(expectedArray, actualArray);
+    }
+    @Test
+    public void upstandingBubbleSortSort() {
+        UpstandingBubbleSort upSort = new UpstandingBubbleSort();
+        fillArrays();
+        Collections.sort(expectedArray);
+        actualArray = upSort.sort(arrayToOperate);
+
+        assertEquals(expectedArray, actualArray);
+    }
+    @Test
+    public void descendingBubbleSort() {
+        DescendingBubbleSort destSort = new DescendingBubbleSort();
+        fillArrays();
+        Collections.sort(expectedArray);
+        actualArray = destSort.sort(arrayToOperate);
+
+        assertEquals(expectedArray, actualArray);
+    }
+}
